7b159dd0869c4ffa1f13fc2c42158dab3c24cf2d
SwampStats <SwampStats@users.noreply.github.com>
2018-01-24 21:57:55 -0600

    fixed typo

--- amusement_park.py
+++ amusement_park.py
@@ -4,6 +4,9 @@ if age < 4:
     price = 0
 elif age < 18:
      price = 5
+elif age < 65:
+    price = 10
 else:
-     price = 10
+    price = 5
+
 print("Your admission cost is $" + str(price) + ".")
